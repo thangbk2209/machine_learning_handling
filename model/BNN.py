61f59273604282df145c102d526ce151678b650f
thangbk2209 <thangbk2209@gmail.com>
2018-08-07 09:07:41 +0700

    last version prediction of BNN

--- model/BNN.py
+++ model/BNN.py
@@ -17,8 +17,8 @@ class Model:
     batch_size = None, num_units_LSTM = None, num_layers = None,
     activation_decoder = None, activation_inference = None, 
     # n_input = None, n_output = None,
-    learning_rate = None, epochs_encoder_decoder = None, epochs_inference = None , input_dim = None, 
-    num_units_inference = None ):
+    learning_rate = None, epochs_encoder_decoder = None, epochs_inference = None , 
+    input_dim = None, num_units_inference = None, patience = None):
         self.original_data = original_data
         self.train_size = train_size
         self.valid_size = valid_size
@@ -38,8 +38,7 @@ class Model:
         self.input_dim = input_dim
 
         self.num_units_inference = num_units_inference
-
-
+        self.patience = patience
     def preprocessing_data(self):
         print ('timeseries1')
         timeseries = TimeseriesBNN(self.original_data, self.train_size, self.valid_size, self.sliding_encoder, self.sliding_decoder, self.sliding_inference, self.input_dim)
@@ -47,7 +46,7 @@ class Model:
         self.train_x_encoder, self.valid_x_encoder, self.test_x_encoder, self.train_x_decoder, self.valid_x_decoder, self.test_x_decoder, self.train_y_decoder, self.valid_y_decoder, self.test_y_decoder, self.min_y, self.max_y, self.train_x_inference, self.valid_x_inference, self.test_x_inference, self.train_y_inference, self.valid_y_inference, self.test_y_inference = timeseries.prepare_data()
     def init_RNN(self, num_units, num_layers):
         cell = tf.contrib.rnn.LSTMCell(num_units,state_is_tuple=True)
-        cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=0.8)
+        cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=0.5)
         rnn_cells = tf.contrib.rnn.MultiRNNCell([cell for _ in range(num_layers)], state_is_tuple = True)
         return rnn_cells
     def early_stopping(self, array, patience):
@@ -100,7 +99,15 @@ class Model:
         out_weights=tf.Variable(tf.random_normal([int(self.sliding_decoder/self.input_dim), self.n_output_encoder_decoder]))
         out_bias=tf.Variable(tf.random_normal([self.n_output_encoder_decoder]))
         # prediction = outputs_decoder[:,:,-1]
-        prediction=tf.nn.sigmoid(tf.matmul(outputs_decoder[:,:,-1],out_weights)+out_bias)
+        if(self.activation_decoder == 1):
+            activation_decode = tf.nn.sigmoid
+        elif(self.activation_decoder == 2):
+            activation_decode = tf.nn.relu 
+        elif(self.activation_decoder == 3):
+            activation_decode = tf.nn.tanh
+        elif(self.activation_decoder == 4):
+            activation_decode = tf.nn.elu
+        prediction=activation_decode(tf.matmul(outputs_decoder[:,:,-1],out_weights)+out_bias)
         prediction_inverse = prediction * (self.max_y - self.min_y) + self.min_y 
         y1_inverse = y1 * (self.max_y - self.min_y) + self.min_y 
         # loss_function
@@ -116,19 +123,23 @@ class Model:
         # state_encoder = tf.reshape(state_encoder[-1].h,  [None, 1, self.num_units])
         # input_inference = tf.concat([x3, state_encoder[-1].h],0)
         if(self.activation_inference == 1):
-            activation = tf.nn.sigmoid
+            activation_infer = tf.nn.sigmoid
         elif(self.activation_inference == 2):
-            activation = tf.nn.relu 
-        hidden_value = tf.layers.dense(input_inference, self.num_units_inference, activation=activation)
-        output_inference = tf.layers.dense(hidden_value,self.n_output_inference, activation=activation)
+            activation_infer = tf.nn.relu 
+        elif(self.activation_inference == 3):
+            activation_infer = tf.nn.tanh
+        elif(self.activation_inference == 4):
+            activation_infer = tf.nn.elu
+        hidden_value = tf.layers.dense(input_inference, self.num_units_inference, activation=activation_infer)
+        output_inference = tf.layers.dense(hidden_value,self.n_output_inference, activation=activation_infer)
         # loss
         loss_inference = tf.reduce_mean(tf.square(y2-output_inference))
         #optimization
         optimizer_inference = tf.train.AdamOptimizer(learning_rate = self.learning_rate).minimize(loss_inference)
         output_inference_inverse = output_inference * (self.max_y - self.min_y) + self.min_y 
         y2_inverse = y2 * (self.max_y - self.min_y) + self.min_y
-        error = tf.reduce_mean(tf.abs(tf.subtract(output_inference_inverse,y2_inverse)) )
-
+        MAE = tf.reduce_mean(tf.abs(tf.subtract(output_inference_inverse,y2_inverse)) )
+        RMSE = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(output_inference_inverse,y2_inverse))))
         cost_train_encoder_decoder_set = []
         cost_valid_encoder_decoder_set = []
         cost_train_inference_set = []
@@ -141,7 +152,7 @@ class Model:
             print ("start training encoder_decoder")
             for epoch in range(self.epochs_encoder_decoder):
                 # Train with each example
-                print ('epoch encoder_decoder: ', epoch)
+                print ('epoch encoder_decoder: ', epoch+1)
                 total_batch = int(len(self.train_x_encoder)/self.batch_size)
                 print (total_batch)
                 # sess.run(updates)
@@ -149,8 +160,6 @@ class Model:
                 for i in range(total_batch):
                     batch_xs_encoder,batch_xs_decoder ,batch_ys = self.train_x_encoder[i*self.batch_size:(i+1)*self.batch_size], self.train_x_decoder[i*self.batch_size:(i+1)*self.batch_size],self.train_y_inference[i*self.batch_size:(i+1)*self.batch_size]
                     sess.run(optimizer_encoder_decoder,feed_dict={x1: batch_xs_encoder,x2: batch_xs_decoder, y1:batch_ys})
-                    # print('state_encoder')
-                    # print (sess.run(state_encoder,feed_dict={x1: batch_xs_encoder}))
                     avg_cost += sess.run(loss_encoder_decoder,feed_dict={x1: batch_xs_encoder,x2: batch_xs_decoder,y1: batch_ys})/total_batch
                     if(i == total_batch -1):
                         a = sess.run(state_encoder,feed_dict={x1: batch_xs_encoder})
@@ -159,8 +168,8 @@ class Model:
                 cost_train_encoder_decoder_set.append(avg_cost)
                 val_cost = sess.run(loss_encoder_decoder, feed_dict={x1:self.valid_x_encoder,x2:self.valid_x_decoder, y1: self.valid_y_decoder})
                 cost_valid_encoder_decoder_set.append(val_cost)
-                if (epoch>5):
-                    if (self.early_stopping(cost_valid_encoder_decoder_set, 5) == False):
+                if (epoch > self.patience):
+                    if (self.early_stopping(cost_valid_encoder_decoder_set, self.patience) == False):
                         print ("early stopping encoder-decoder training")
                         break
                 print ("Epoch encoder-decoder finished")
@@ -168,7 +177,7 @@ class Model:
             # training inferences
             print ('start training inference')
             for epoch in range(self.epochs_inference):
-                print ("epoch inference: ", epoch)
+                print ("epoch inference: ", epoch+1)
                 total_batch = int(len(self.train_x_inference)/self.batch_size)
                 print (total_batch)
                 # sess.run(updates)
@@ -186,22 +195,30 @@ class Model:
                 # epoch_set.append(epoch+1)
                 val_cost = sess.run(loss_inference, feed_dict={x1:self.valid_x_encoder,x3:self.valid_x_inference, y2: self.valid_y_inference})
                 cost_valid_inference_set.append(val_cost)
-                if (epoch>5):
-                    if (self.early_stopping(cost_valid_inference_set,5) == False):
-                        print ("early stopping encoder-decoder training")
+                if (epoch > self.patience):
+                    if (self.early_stopping(cost_valid_inference_set , self.patience) == False):
+                        print ("early stopping inference training")
                         break
             output_inference_inverse = sess.run(output_inference_inverse, feed_dict={x1:self.test_x_encoder,x3:self.test_x_inference, y2: self.test_y_inference})
             output_inference = sess.run(output_inference, feed_dict={x1:self.test_x_encoder,x3:self.test_x_inference, y2: self.test_y_inference})
             # print (output_inference)
-            error = sess.run(error, feed_dict={x1:self.test_x_encoder,x3:self.test_x_inference, y2: self.test_y_inference})
-            print (error)
+            vector_state = sess.run(state_encoder[-1].h,feed_dict={x1:self.test_x_encoder})
+            MAE = sess.run(MAE, feed_dict={x1:self.test_x_encoder,x3:self.test_x_inference, y2: self.test_y_inference})
+            RMSE = sess.run(RMSE, feed_dict={x1:self.test_x_encoder,x3:self.test_x_inference, y2: self.test_y_inference})
+            print ('MAE: ', MAE)
+            print ('RMSE: ', RMSE)
+            error = [MAE,RMSE]
             # print (output_inference_inverse)
             # print (self.test_y_inference)
             # print (len(output_inference_inverse))
             # print (len(self.test_y_inference))
             
-        
-            
+            folder_to_save_result = 'results/mem/5minutes/'
+            history_file = folder_to_save_result + 'history' + str(self.sliding_encoder) + '-' + str(self.sliding_decoder) + '-' + str(self.sliding_inference) + '-' + str(self.batch_size) + '-' + str(self.num_units_LSTM) + '-' + str(self.num_layers) + '-' + str(self.activation_decoder) + '-' + str(self.activation_inference) + '-' + str(self.input_dim) + '-' + str(self.num_units_inference) + '.png'
+            error_file = folder_to_save_result + 'error' + str(self.sliding_encoder) + '-' + str(self.sliding_decoder) + '-' + str(self.sliding_inference) + '-' + str(self.batch_size) + '-' + str(self.num_units_LSTM) + '-' + str(self.num_layers) + '-' + str(self.activation_decoder) + '-' + str(self.activation_inference) + '-' + str(self.input_dim) + '-' + str(self.num_units_inference) + '.csv'
+            prediction_file = folder_to_save_result + 'predict' + str(self.sliding_encoder) + '-' + str(self.sliding_decoder) + '-' + str(self.sliding_inference) + '-' + str(self.batch_size) + '-' + str(self.num_units_LSTM) + '-' + str(self.num_layers) + '-' + str(self.activation_decoder) + '-' + str(self.activation_inference) + '-' + str(self.input_dim) + '-' + str(self.num_units_inference) + '.csv'
+            vector_state_file = folder_to_save_result + 'vector_presentation' + str(self.sliding_encoder) + '-' + str(self.sliding_decoder) + '-' + str(self.sliding_inference) + '-' + str(self.batch_size) + '-' + str(self.num_units_LSTM) + '-' + str(self.num_layers) + '-' + str(self.activation_decoder) + '-' + str(self.activation_inference) + '-' + str(self.input_dim) + '-' + str(self.num_units_inference) + '.csv'
+
             plt.plot(cost_train_inference_set)
             plt.plot(cost_valid_inference_set)
             plt.title('model loss')
@@ -209,10 +226,15 @@ class Model:
             plt.xlabel('epoch')
             plt.legend(['train', 'validation'], loc='upper left')
             # plt.show()
-            plt.savefig('/home/thangnguyen/hust/lab/machine_learning_handling/history/history_mem.png')
+            # plt.savefig('/home/thangnguyen/hust/lab/machine_learning_handling/history/history_mem.png')
+            plt.savefig(history_file)
 					
             predictionDf = pd.DataFrame(np.array(output_inference_inverse))
-            predictionDf.to_csv('/home/thangnguyen/hust/lab/machine_learning_handling/results/result_mem.csv', index=False, header=None)
-            # errorDf = pd.DataFrame(np.array(error))
-            # errorDf.to_csv('/home/thangnguyen/hust/lab/machine_learning_handling/results/error.csv', index=False, header=None)
+            # predictionDf.to_csv('/home/thangnguyen/hust/lab/machine_learning_handling/results/result_mem.csv', index=False, header=None)
+            predictionDf.to_csv(prediction_file, index=False, header=None)
+            errorDf = pd.DataFrame(np.array(error))
+            errorDf.to_csv(error_file, index=False, header=None)
+            errorDf.to_csv(prediction_file, index=False, header=None)
+            vector_stateDf = pd.DataFrame(np.array(vector_state))
+            vector_stateDf.to_csv(vector_state_file, index=False, header=None)
             sess.close()
--- train_encoder_decoder.py
+++ train_encoder_decoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 from multiprocessing import Pool
-# from queue import Queue
+from queue import Queue
 from sklearn.model_selection import ParameterGrid
 
 from sklearn import datasets
@@ -19,8 +19,65 @@ from model.BNN import Model as BNN
 
 # queue = Queue()
 
-link = './data/google_trace_timeseries/data_resource_usage_5Minutes_6176858948.csv'
+# link = './data/google_trace_timeseries/data_resource_usage_10Minutes_6176858948.csv'
 
+# colnames = ['cpu_rate','mem_usage','disk_io_time','disk_space'] 
+# df = read_csv(link, header=None, index_col=False, names=colnames, usecols=[3,4,9,10], engine='python')
+# scaler = MinMaxScaler(feature_range=(0, 1))
+# cpu = df['cpu_rate'].values.reshape(-1,1)
+# mem = df['mem_usage'].values.reshape(-1,1)
+# disk_io_time = df['disk_io_time'].values
+# disk_space = df['disk_space'].values
+
+
+
+# sliding_encoders = [18]
+# sliding_decoders = [2]
+# sliding_inferences = [1]
+# # activation for inference layer : - 1 is sigmoid
+# #                                  - 2 is relu
+# #                                  - 3 is softmax 
+# activation_inferences = [1]
+# # num_units_LSTM_arr - array number units lstm for encoder and decoder
+# num_units_LSTM_arr = [4]
+# num_units_inference_arr = [20]
+
+# batch_size_arr = [4]
+
+# metrics = [1]
+# activation_decoder = 1
+# num_layers = 1
+# input_dim = 1
+# n_output_encoder_decoder = 1
+
+queue = Queue()
+
+def train_model(item):
+    sliding_encoder = item["sliding_encoder"]
+    sliding_decoder = item["sliding_decoder"]
+    sliding_inference = item["sliding_inference"]
+    batch_size = item["batch_size"]
+    num_units_LSTM = item["num_unit_LSTM"]
+    num_layer = item["num_layer"]
+    activation_decoder = item["activation_decoder"]
+    activation_inference = item["activation_inference"]
+    input_dim = item["input_dim"]
+    num_units_inference = item["num_units_inference"]    
+
+    model = BNN(dataset_original, train_size, valid_size, 
+        sliding_encoder =  sliding_encoder, sliding_decoder = sliding_decoder,
+        sliding_inference = sliding_inference, batch_size = batch_size,
+        num_units_LSTM = num_units_LSTM, num_layers = num_layer, 
+        activation_decoder = activation_decoder, activation_inference = activation_inference, 
+        learning_rate = learning_rate, epochs_encoder_decoder = epochs_encoder_decoder,
+        epochs_inference = epochs_inference,
+        input_dim = input_dim, num_units_inference = num_units_inference, patience = patience )
+    model.fit()
+
+
+# producer
+# define constant
+link = './data/google_trace_timeseries/data_resource_usage_5Minutes_6176858948.csv'
 colnames = ['cpu_rate','mem_usage','disk_io_time','disk_space'] 
 df = read_csv(link, header=None, index_col=False, names=colnames, usecols=[3,4,9,10], engine='python')
 scaler = MinMaxScaler(feature_range=(0, 1))
@@ -29,75 +86,55 @@ mem = df['mem_usage'].values.reshape(-1,1)
 disk_io_time = df['disk_io_time'].values
 disk_space = df['disk_space'].values
 
-# define constant
+dataset_original = mem
 train_size = int(0.6 * len(cpu))
-print (train_size)
+# print (train_size)
 valid_size = int(0.2 * len(cpu))
 
-sliding_encoders = [18,24,30]
-slding_decoders = [2,3,4,5,6]
-sliding_inferences = [1,2,3,4,5]
-# activation for inference layer : - 1 is sigmoid
-#                                  - 2 is relu
-#                                  - 3 is softmax 
-activation_inferences = [1,2]
-# num_units_LSTM_arr - array number units lstm for encoder and decoder
-num_units_LSTM_arr = [4, 8, 16, 32]
 
-batch_size_arr = [4, 8, 16, 32, 64, 128]
-num_units_inference_arr = [4, 8, 16, 64]
-metrics = [1,2]
-
-activation_decoder = 1
-num_layers = 1
+sliding_encoders = [18,21]
+sliding_decoders = [2,3,4,5]
+sliding_inferences = [10]
+batch_size_arr = [4,8,32,64,128]
+num_units_LSTM_arr = [4]
+num_layers = [1]
+# activation for inference and decoder layer : - 1 is sigmoid
+#                                              - 2 is relu
+#                                              - 3 is tanh
+#                                              - 4 is elu
+activation_decoder = [1,2,3,4]
+activation_inferences = [1,2,3,4]
 learning_rate = 0.01
 epochs_encoder_decoder = 2000
 epochs_inference = 2000
-input_dim = [1,2]
-n_output_encoder_decoder = 1
+patience = 20  #number of epoch checking for early stopping
+# num_units_LSTM_arr - array number units lstm for encoder and decoder
+input_dim = [1]
+num_units_inference_arr = [20]
 
-for metric in metrics:
-    for sliding_encoder in sliding_encoders:
-        for sliding_decoder in slding_decoders:
-            for sliding_inference in sliding_inferences:
-                for activation_inference in activation_inferences:
-                    for num_units_LSTM in num_units_LSTM_arr:
-                        for batch_size in batch_size_arr:
-                            for num_units_inference in num_units_inference_arr:
-                                if(metric==1):
-                                    print('mem1')
-                                    model = BNN(mem, train_size, valid_size, 
-                                        sliding_encoder, sliding_decoder, sliding_inference, batch_size,
-                                        num_units_LSTM, num_layers, activation_decoder, activation_inference,
-                                        # n_input = None, n_output = None,
-                                        learning_rate, epochs_encoder_decoder,epochs_inference, input_dim, num_units_inference )
-                                    model.fit()
-                                else:
-                                    model = BNN(cpu, train_size, valid_size, 
-                                        sliding_encoder, sliding_decoder, sliding_inference, batch_size,
-                                        num_units, num_layers, activation_decoder, activation_inference,
-                                        # n_input = None, n_output = None,
-                                        learning_rate, epochs_encoder_decoder,epochs_inference, input_dim, num_units_inference )
-                                    model.fit()
-
-# sliding_encoder = 20
-# sliding_decoder = 4
-# sliding_inference = 10
-# activation_decoder = 1
-# activation_inference = 1
-# num_units = 4
-# num_layers = 1
-# learning_rate = 0.01
-# epochs_encoder_decoder = 20
-# epochs_inference = 20
-# input_dim = 1
 # n_output_encoder_decoder = 1
-# batch_size = 4
-# num_units_inference = 20
-
-# model = BNN(mem, train_size, valid_size, 
-#     sliding_encoder, sliding_decoder, sliding_inference, batch_size,
-#     num_units, num_layers, activation_decoder, activation_inference,
-#     # n_input = None, n_output = None,
-#     learning_rate, epochs_encoder_decoder,epochs_inference, input_dim, num_units_inference )
-# model.fit()
+param_grid = {
+        'sliding_encoder': sliding_encoders,
+        'sliding_decoder': sliding_decoders,
+        'sliding_inference': sliding_inferences,
+        'batch_size': batch_size_arr,
+        'num_unit_LSTM': num_units_LSTM_arr,
+        'num_layer': num_layers,
+        'activation_decoder': activation_decoder,
+        'activation_inference': activation_inferences,
+        'input_dim': input_dim,
+        'num_units_inference': num_units_inference_arr
+    }
+# Create combination of params.
+print ("check")
+a = ParameterGrid(param_grid)
+print(type(a))
+# print (a.__len__())
+for item in list(ParameterGrid(param_grid)) :
+    queue.put_nowait(item)
+# Consumer
+pool = Pool(8)
+pool.map(train_model, list(queue.queue))
+pool.close()
+pool.join()
+pool.terminate()
