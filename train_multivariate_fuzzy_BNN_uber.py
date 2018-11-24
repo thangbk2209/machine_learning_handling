cc79cccfa2093bcf744f84144b179b2341fed672
thangbk2209 <thangbk2209@gmail.com>
2018-11-25 00:16:14 +0700

    experiment uni mem

--- train_multivariate_fuzzy_BNN_uber.py
+++ train_multivariate_fuzzy_BNN_uber.py
@@ -60,7 +60,7 @@ def train_model(item):
             name_inference += str(num_units_inference[i]) +'_'
     file_name = str(sliding_encoder) + '-' + str(sliding_decoder) + '-' + str(sliding_inference) + '-' + str(batch_size) + '-' + name_LSTM + '-' + str(activation)+ '-' + str(optimizer) + '-' + str(input_dim) + '-' + name_inference +'-'+str(number_out_decoder) +'-'+str(dropout_rate)
             
-    summary = open("results/multivariate/mem/5minutes/evaluate_multivariate_bnn_uber_ver6.csv",'a+')
+    summary = open("results/univariate/mem/5minutes/evaluate_multivariate_bnn_uber_ver2.csv",'a+')
     summary.write(file_name +','+str(error[0])+','+str(error[1])+'\n')
     print (error)
     # except:
@@ -83,7 +83,7 @@ fuzzied_cpu = fuzzy_df['cpu_rate'].values.reshape(-1,1)
 fuzzied_mem = fuzzy_df['mem_usage'].values.reshape(-1,1)
 fuzzied_disk_io_time = fuzzy_df['disk_io_time'].values.reshape(-1,1)
 fuzzied_disk_space = fuzzy_df['disk_space'].values.reshape(-1,1)
-dataset_original = [mem, cpu]
+dataset_original = [mem]
 prediction_data = [mem]
 external_feature = [mem]
 
@@ -142,7 +142,7 @@ for item in list(ParameterGrid(param_grid)) :
     queue.put_nowait(item)
 # Consumer
 if __name__ == '__main__':
-    summary = open("results/multivariate/mem/5minutes/evaluate_multivariate_bnn_uber_ver6.csv",'a+')
+    summary = open("results/univariate/mem/5minutes/evaluate_multivariate_bnn_uber_ver2.csv",'a+')
     summary.write("model,MAE,RMSE\n")
  
     pool = Pool(8)
